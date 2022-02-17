47a4c5c25d69c58adc7c922adb9d8167f191b82d
SmeraldaKa0s <anabella.lerner@gmail.com>
2022-02-17 17:33:54 -0300

    agrego input

--- src/App.js
+++ src/App.js
@@ -3,23 +3,32 @@ import './App.css';
 import { useState, useEffect } from 'react'; 
 
 const App = () => {
-  const [episodios, setEpisodios] = useState([])
-  const [paginas, setPaginas] = useState(0);
   //se pueden usar 2 estados para pedir información distinta al mismo fetch
+  const [episodios, setEpisodios] = useState([])
+  const [info, setInfo] = useState({});
+  const [data, setData] = useState ({}); 
+  const [busqueda, setBusqueda] = useState (""); //poniendo string vacio me trae la busqueda general de los episodios, esta api me deja, otras se romperian 
 
   useEffect(()=>{
-    fetch("https://rickandmortyapi.com/api/episode/")
+    fetch(`https://rickandmortyapi.com/api/episode?name=${busqueda}`)
     .then(res => res.json())
     .then((data) => {
-      setEpisodios(data.results)
-      setPaginas(data.info.pages);
+      setEpisodios(data.results);
+      setData(data);
     });
-  },[])
+  },[busqueda])
 
-  console.log(episodios);
+  const handleChangeInput = e => setBusqueda(e.target.value);
+
+  console.log("Se renderizó el componente App");
   return (
     <div>
-      Cantidad de paginas: {paginas}
+      <div>
+        <label>Busca un episodio de Rick and Morty</label>
+        <input type="text" onChange={handleChangeInput} />
+      </div>
+
+      Cantidad de paginas: {data?.info?.pages}
       {episodios.map((episodio) => (
         <h2>{episodio.name}</h2>
       ))}
---
+++ src/components/Input.js
@@ -0,0 +1,16 @@
+// import { useState } from "react"; 
+
+
+// const Input = () => {
+//   const [busqueda, setBusqueda] = useState (""); //poniendo string vacio me trae la busqueda general de los episodios, esta api me deja, otras se romperian 
+//   const handleChangeInput = e => setBusqueda(e.target.value);
+
+//   return (
+//     <div>
+//       <label>Busca un episodio de Rick and Morty</label>
+//       <input type="text" onChange={handleChangeInput} />
+//     </div>
+//   )
+// }
+
+// export default Input 
